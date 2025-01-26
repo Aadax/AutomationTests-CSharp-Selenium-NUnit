e8cc7b5c52c96af7cd512253e65f159be326ce8e
Aadax <a.w.abramczyk@gmail.com>
2025-01-26 01:12:50 +0100

    Add project files.

---
+++ AutomationTests-CSharp-Selenium-NUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AutomationTests_CSharp_Selenium_NUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
