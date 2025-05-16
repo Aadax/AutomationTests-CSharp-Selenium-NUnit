335876a893c8873285dbe1a84aecf4e164a631d3
Aadax <a.w.abramczyk@gmail.com>
2025-05-17 01:38:38 +0200

    AT-9 - Login extended/additional generic methods

---
+++ AutomationTests-CSharp-Selenium-NUnit/Fixture/Helpers/StepHelper.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace AutomationTests_CSharp_Selenium_NUnit
+{
+    public static class StepHelper
+    {
+        public static void Step(string message)
+        {
+            TestContext.Progress.WriteLine(message);
+        }
+    }
+}
--- AutomationTests-CSharp-Selenium-NUnit/Tests/LoginTests/LoginTests.cs
+++ AutomationTests-CSharp-Selenium-NUnit/Tests/LoginTests/LoginTests.cs
@@ -3,15 +3,19 @@
 namespace AutomationTests_CSharp_Selenium_NUnit.PageObjectPattern
 {
     [TestFixture]
+    [Category("Login Tests")]
     public class LoginTests : Base
     {
-        //[TestCaseSource(typeof(LoginData), "Login")]
-        [Test]
+        [TestCase(TestName = "Login Authorization")]
+        [Description("User is able to log in to the app.")]
         public void TestMethod()
         {
             LoginPO login = new LoginPO(Driver);
+            Step("User enter nickname");
             login.Username.SendKeys("Admin");
+            Step("User enter password");
             login.Password.SendKeys("admin123");
+            Step("User click on login button");
             login.LoginButtonClick();
         }
     }
