bbe024807932c3e1b162d70553f8d9058bc3f5e7
Aadax <a.w.abramczyk@gmail.com>
2025-11-22 04:50:35 +0100

    added json file config added method of login to avoid code duplication changes fit ignore - added: .project .classpath .settings/
    Changed recent names in LoginPage
    
    added method  helper to LoginPage
    Added new Folder to DataModels: LoginCredentials

---
+++ AutomationTests-CSharp-Selenium-NUnit/Fixture/DataModels/LoginCredentials/LoginCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace AutomationTests_CSharp_Selenium_NUnit.Fixture.DataModels.LoginCredentials
+{
+    public class LoginCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
